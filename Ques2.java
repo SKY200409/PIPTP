16bb2d343d17633405a2b8b9f7329a61b30add8e
SKY200409 <yadavsudheer5295@gmail.com>
2025-07-31 14:12:12 +0530

    Add files via upload

---
+++ Ques2.java
@@ -0,0 +1,11 @@
+public class Ques2 {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if (x > 0 && x % 2 == 1) {
+            System.out.println("Positive odd");
+        } else {
+            System.out.println("Other");
+        }
+    }
+}
